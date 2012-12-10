21de8363434c49431241e55b5d25e8f13445e68d
jackson henry <jacksonhenry3@gmail.com>
2012-12-10 15:15:51 -0500

    refectord code

---
+++ ChunkedRSA.py
@@ -0,0 +1,48 @@
+#simply to automate some stuff.
+def exploop(array,power,N):
+	newarray = []
+	for i in array:
+		newarray.append(int(i**power%N))
+	return(newarray)
+
+#generates private key
+def GeneratePrivateKey(P,Q,e):
+	N = P*Q
+	PhiN = (P-1)*(Q-1)
+	k = 2
+	d = (k*PhiN+1)/e%N
+	return(d)
+
+#encrypts a message
+def encrypt(Message,e,N):
+	encryptedMessage = exploop(Message,e,N)
+	return(encryptedMessage)
+
+#decrypt a message
+def decrypt(encryptedMessage,d,N):
+	 Message = exploop(encryptedMessage,d,N)
+	 return(Message)
+
+def Numerify(message):
+	array = ["""1""",""" ""","""2""","""3""","""4""","""5""","""6""","""7""","""8""","""9""","""0""","""q""","""w""","""e""","""r""","""t""","""y""","""u""","""i""","""o""","""p""","""a""","""s""","""d""","""f""","""g""","""h""","""j""","""k""","""l""","""z""","""x""","""c""","""v""","""b""","""n""","""m""","""Q""","""W""","""E""","""R""","""T""","""Y""","""U""","""I""","""O""","""P""","""A""","""S""","""D""","""F""","""G""","""H""","""J""","""K""","""L""","""Z""","""X""","""C""","""V""","""B""","""N""","""M""","""~""","""!""","""@""","""#""","""$""","""%""","""^""","""&""","""*""","""_""","""+""","""-""","""=""","""[""","""]""","""\\""","""{""","""}""","""|""",""":""",""" " """,""":""","""'""","""<""",""">""","""?""",""",""",""".""","""/"""]
+	dictionary = {}
+	for j in range(len(array)):
+		dictionary[array[j]] = j
+
+	NumericMessage = []
+	for i in message:
+		NumericMessage.append(dictionary[i])
+
+	return(NumericMessage)
+
+def Denumerificate(NumericMessage):
+	array = ["""1""",""" ""","""2""","""3""","""4""","""5""","""6""","""7""","""8""","""9""","""0""","""q""","""w""","""e""","""r""","""t""","""y""","""u""","""i""","""o""","""p""","""a""","""s""","""d""","""f""","""g""","""h""","""j""","""k""","""l""","""z""","""x""","""c""","""v""","""b""","""n""","""m""","""Q""","""W""","""E""","""R""","""T""","""Y""","""U""","""I""","""O""","""P""","""A""","""S""","""D""","""F""","""G""","""H""","""J""","""K""","""L""","""Z""","""X""","""C""","""V""","""B""","""N""","""M""","""~""","""!""","""@""","""#""","""$""","""%""","""^""","""&""","""*""","""_""","""+""","""-""","""=""","""[""","""]""","""\\""","""{""","""}""","""|""",""":""",""" " """,""":""","""'""","""<""",""">""","""?""",""",""",""".""","""/"""]
+	dictionary = {}
+	for j in range(len(array)):
+		dictionary[j] = array[j]
+
+	Message = ''
+	for i in NumericMessage:
+		Message+=str(dictionary[i])
+
+	return(Message)
--- RSA.py
+++ RSA.py
@@ -1,42 +1,16 @@
-#simply to automate some stuff.
-def exploop(array,power):
-	newarray = []
-	for i in array:
-		newarray.append(int(i**power%N))
-	return(newarray)
+Message = "hello"
 
-############
-#KEY GENERATION
-def Phi(P,Q):
-	"""where N = P*Q"""
-	return((P-1)*(Q-1))
+from ChunkedRSA import *
 
-#these are two random prime numbers
-P = 53
-Q = 59
-PhiN = Phi(P,Q)
-
-# this is the PUBLIC key
-#################
+[P,Q,e] = [53,59,3]
 N = P*Q
-#any small value that DOES NOT share a factor with Phi(n)
-e = 3
-################
 
-#this is a PRIVATE value (the backdoor exponent)
-k=2
-d = (k*PhiN+1)/e%N
+publicKey = [N,e]
 
-#d< (p-1)(q-1) s.t. de==1 mod (p-1)(q-1).
+privateKey = GeneratePrivateKey(P,Q,e)
 
+NumericalMessage = Numerify(Message)
 
-############
-#MESSAGE ENCRYPTION
-Message = [3,1,4,2,5,6,7,9,0,76,5,4,3,5,8]
-
-EncryptedMessage = exploop(Message,e)
-
-#############
-#MESSAGE DECRYPTION
-DecryptedMessage = exploop(EncryptedMessage,d)
-print(DecryptedMessage)
+encryptedMessage = encrypt(NumericalMessage,e,P*Q)
+M = decrypt(encryptedMessage,privateKey,N)
+print(Denumerificate(M))
